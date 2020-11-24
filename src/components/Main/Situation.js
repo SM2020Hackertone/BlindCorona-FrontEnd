02fa3745704d19db09f85c25f7eef10b499ba7df
Choi-jinwoo <chlwlsdn0828@gmail.com>
2020-11-25 05:14:52 +0900

    Fix: axios web url

--- src/components/Main/Situation.js
+++ src/components/Main/Situation.js
@@ -1,4 +1,4 @@
-import { useEffect,useState } from "react"
+import { useEffect, useState } from "react"
 import "./main.scss"
 import { GetStatus } from "../../actions/userAction"
 import SituationCard from "./SituationCard"
@@ -21,51 +21,51 @@ import { useDispatch, useSelector } from "react-redux";
     stdDay: "2020년 11월 23일 00시",
     updateDt: "null"
 }*/
-function Situation({props}) {
-    const dispatch=useDispatch();
-    const [data,setData] = useState(null);
-    const [loading,setLoading] = useState(false);
-    useEffect(()=>{
+function Situation({ props }) {
+    const dispatch = useDispatch();
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
         setLoading(true);
-        if(props.text){
-            axios.get(`http://172.30.1.58:5000/corona-info?text=${props.text}`)
-            .then((res)=>{
-                setData(res.data.data);
-                console.log(res.data.data);
-                window.speechSynthesis.cancel();
+        if (props.text) {
+            axios.get(`http://192.168.0.156:5000/corona-info?text=${props.text}`)
+                .then((res) => {
+                    setData(res.data.data);
+                    console.log(res.data.data);
+                    window.speechSynthesis.cancel();
 
-                const speechMsg = new SpeechSynthesisUtterance();
-                speechMsg.text=props.text+"에 대한 검색결과 입니다." + res.data.data.gubun + " " + 
-                "신규 확진자수" + res.data.data.incDec + "명 " + 
-                "사망자 수" + res.data.data.deathCnt + "명 " +
-                "누적 확진률" + res.data.data.qurRate + "%";
-                
-                console.log('pass') 
-                console.log(window.speechSynthesis.speak(speechMsg));  
-            })
-            .catch((e)=>{
-                window.speechSynthesis.cancel();
-                const speechMsg = new SpeechSynthesisUtterance();
-                speechMsg.text="결과를 찾을수 없습니다 다시 말해주세요."
-                window.speechSynthesis.speak(speechMsg);
-            })
-        }else{
+                    const speechMsg = new SpeechSynthesisUtterance();
+                    speechMsg.text = props.text + "에 대한 검색결과 입니다." + res.data.data.gubun + " " +
+                        "신규 확진자수" + res.data.data.incDec + "명 " +
+                        "사망자 수" + res.data.data.deathCnt + "명 " +
+                        "누적 확진률" + res.data.data.qurRate + "%";
+
+                    console.log('pass')
+                    console.log(window.speechSynthesis.speak(speechMsg));
+                })
+                .catch((e) => {
+                    window.speechSynthesis.cancel();
+                    const speechMsg = new SpeechSynthesisUtterance();
+                    speechMsg.text = "결과를 찾을수 없습니다 다시 말해주세요."
+                    window.speechSynthesis.speak(speechMsg);
+                })
+        } else {
             window.speechSynthesis.cancel();
             const speechMsg = new SpeechSynthesisUtterance();
-            speechMsg.text="마이크를 사용하시려면 m버튼을 눌러주세요";
+            speechMsg.text = "마이크를 사용하시려면 m버튼을 눌러주세요";
             window.speechSynthesis.speak(speechMsg);
-            dispatch(GetStatus("",""))
-                .then((res)=>{
+            dispatch(GetStatus("", ""))
+                .then((res) => {
                     setData(res.payload);
                     console.log(res);
                 })
         }
         setLoading(false);
-    },[])
-    if(loading) return(<div>로딩중..</div>)
-    if(!data) return(null);
-    
-    return(
+    }, [])
+    if (loading) return (<div>로딩중..</div>)
+    if (!data) return (null);
+
+    return (
         <>
             <div>
                 <h3 className="situation-title">{data.gubun}</h3>
@@ -77,9 +77,9 @@ function Situation({props}) {
                     <SituationCard unit={data.isolIngCnt} dataType="검사진행 수"></SituationCard>
                     <SituationCard unit={data.deathCnt} dataType="사망자 수"></SituationCard>
                     <SituationCard unit={data.careCnt} dataType="치료중인 환자 수"></SituationCard>
-                    <SituationCard unit={data.accExamCnt} dataType="누적 검사 수"></SituationCard>  
+                    <SituationCard unit={data.accExamCnt} dataType="누적 검사 수"></SituationCard>
                     <SituationCard unit={data.resutlNegCnt} dataType="결과 음성 수"></SituationCard>
-                    <SituationCard unit={data.qurRate} dataType="누적 확진률"></SituationCard>   
+                    <SituationCard unit={data.qurRate} dataType="누적 확진률"></SituationCard>
                 </div>
             </div>
         </>
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const DOMAIN = "http://localhost:5000";
+const DOMAIN = "http://192.168.0.156:5000";
 axios.defaults.withCredentials = false;
 export const request = (method, url, headers, data) => {
   return axios({
