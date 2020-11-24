52c0e708fd16894d0686db61099f1ff6fcde0275
Choi-jinwoo <chlwlsdn0828@gmail.com>
2020-11-25 05:54:59 +0900

    Fix: UI

--- src/components/Main/SituationCard.js
+++ src/components/Main/SituationCard.js
@@ -1,18 +1,18 @@
 import CountUp from 'react-countup';
 import { useSelector } from 'react-redux';
 
-function SituationCard({unit, dataType}){
-    function clickEvent(){
+function SituationCard({ unit, dataType }) {
+    function clickEvent() {
         const speechMsg = new SpeechSynthesisUtterance();
-        speechMsg.text=dataType + unit + "명";
-        
+        speechMsg.text = dataType + unit + "명";
+
         window.speechSynthesis.speak(speechMsg)
     }
     const DarkMode = useSelector(state => state.user.darkmode);
-    return(
+    return (
         <div onClick={clickEvent}>
-            <h3 id="Unit" style={DarkMode? {color:"white"} : {color: "black"}}><CountUp duration={0.6} separator="," end={unit}></CountUp> / <b>{ dataType=="누적 확진률"? "%" : "명"}</b></h3>
-            <p style={DarkMode? {color:"white"} :  {color: "black"}} id="dataType">{dataType}</p>
+            <h3 id="Unit" style={DarkMode ? { color: "white" } : { color: "black" }}><CountUp duration={0.6} separator="," end={unit}></CountUp><b>{dataType == "누적 확진률" ? "%" : "명"}</b></h3>
+            <p style={DarkMode ? { color: "white" } : { color: "black" }} id="dataType">{dataType}</p>
         </div>
     )
 }
