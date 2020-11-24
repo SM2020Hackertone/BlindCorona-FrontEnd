c5358c80f08a9798ec935ca9e2238db38e4c4248
eungyeole <66717787+eungyeole@users.noreply.github.com>
2020-11-25 05:51:19 +0900

    스크롤 tts 오류수정

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -10,14 +10,17 @@ function Test({ match }) {
         console.log(e.nativeEvent.wheelDelta);
         if (e.nativeEvent.wheelDelta <= 0) {
             document.getElementById("main").style.transform = "translate(0, -100vh)";
-            window.speechSynthesis.cancel();
             const speechMsg = new SpeechSynthesisUtterance();
             console.log(news);
             news.news.map((i, index) => {
                 speechMsg.text += "\n\n" + (index + 1) + i.title;
             })
             console.log(speechMsg);
-            window.speechSynthesis.speak(speechMsg)
+            if (document.getElementsByClassName("news-list")[0].scrollTop== 0) { 
+                window.speechSynthesis.cancel();   
+                window.speechSynthesis.speak(speechMsg)
+                document.getElementsByClassName("news-list")[0].scrollTop=1;
+            }
         }
         else {
             if (document.getElementsByClassName("news-list")[0].scrollTop == 0) {
